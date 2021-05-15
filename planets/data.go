2f687407164f3fe181e41b9df2e7f3b6797af50b
victor UDOH <vicudoh345@gmail.com>
2021-05-16 00:03:40 +0100

    adds first go file

---
+++ planets/data.go
@@ -0,0 +1,12 @@
+package planets
+
+var planetsName = map[string]int{
+	"Mercury": 58000000,
+	"Venus":   108000000,
+	"Earth":   150000000,
+	"Mars":    228000000,
+	"Jupiter": 778000000,
+	"Saturn":  14000000000,
+	"Uranus":  29000000000,
+	"Neptune": 45000000000,
+}
